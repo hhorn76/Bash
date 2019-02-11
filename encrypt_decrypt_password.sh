8eb471f72098d266f0b1818cf8298afb62d5a59b
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2019-02-11 22:51:11 +0100

    Update encrypt_decrypt_password.sh

--- encrypt_decrypt_password.sh
+++ encrypt_decrypt_password.sh
@@ -3,18 +3,47 @@
 
 strPassword='8yaThaDCTGHxG8fu'
 
+##########################################################
+echo "Password: ${strPassword}"
+echo ''
+
+##########################################################
+## Using base64
 # Function to encrypt password
-encryptPw (){
-	myEncryptedPassword=$(echo $1 | base64)
+encryptPwBase64 (){
+	strEncrypted=$(echo $1 | base64)
 }
 
-# Function to encrypt password
-decryprPw () {
-	myDecryptedPassword=$(echo "${1}" | base64 --decode)
+# Function to decrypt password
+decryprPwBase64 () {
+	strDecrypted=$(echo "${1}" | base64 --decode)
 }
 
-encryptPw '8yaThaDCTGHxG8fu'
-echo "My Encrypted Password is: $myEncryptedPassword"
+encryptPwBase64 ${strPassword}
+echo "Base64 encrypted password: ${strEncrypted}"
 
-decryprPw $myEncryptedPassword
-echo "My Decrypted Password is: $myDecryptedPassword"
+decryprPwBase64 ${strEncrypted}
+echo "Base64 decrypted password: ${strDecrypted}"
+echo ''
+
+##########################################################
+## Using openssl
+# Function to encrypt password
+function encryptPwOpenSSL() {
+	local strPassword="${1}"
+	strSalt=$(openssl rand -hex 8)
+	strPassphrase=$(openssl rand -hex 12)
+	strEncrypted=$(echo "${strPassword}" | openssl enc -aes256 -a -A -S "${strSalt}" -k "${strPassphrase}")	
+}
+# Function to decrypt password
+function decryptPwOpenSSL() {
+	# Usage: ~$ DecryptString "Encrypted String" "Salt" "Passphrase"
+	echo "${1}" | /usr/bin/openssl enc -aes256 -d -a -A -S "${2}" -k "${3}"
+}
+
+encryptPwOpenSSL ${strPassword}
+echo "OpenSSL encrypted password: ${strEncrypted}"
+echo "Salt: ${strSalt} | Passphrase: ${strPassphrase}"
+
+strDecrypted=$(decryptPwOpenSSL "${strEncrypted}" "${strSalt}" "${strPassphrase}") 
+echo "OpenSSL encrypted password: ${strDecrypted}"
