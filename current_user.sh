5c95c41f75eee048bcec9de014415437098878ec
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2019-03-27 23:45:36 +0100

    Update current_user.sh

--- current_user.sh
+++ current_user.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 # written by Heiko 11.02.2019
 
-function currentUser {
+function currentUserLast {
     currentUser=$( /usr/bin/last -1 -t console | awk '{print $1}' )
     echo $currentUser
 }
 
-currentUser
+function currentUserWho {
+    currentUser=$( /usr/bin/who | grep 'console' | awk '{print $1}' )
+    echo $currentUser
+}
+
+function currentUserLs {
+    currentUser=$( /bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }' )
+    echo $currentUser
+}
+
+function currentUserStat {
+    currentUser=$( /usr/bin/stat -f%Su /dev/console )
+    echo $currentUser
+}
+
+function currentUserDefaults {
+    currentUser=$( /usr/bin/defaults read /Library/Preferences/com.apple.loginwindow.plist lastUserName )
+    echo $currentUser
+}
+
+function currentUserPythonSysConfig {
+    currentUser=$( /usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
+    echo $currentUser
+}
+
+currentUserLast
+currentUserWho
+currentUserLs
+currentUserStat
+currentUserDefaults
+currentUserPythonSysConfig
